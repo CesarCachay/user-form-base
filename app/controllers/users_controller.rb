cff84172f9ceb1aa4b64fc41a28a66ab9de20cf0
Cesar Cachay <cesarcachay1996@gmail.com>
2019-04-12 09:41:51 -0500

    Add comments to the methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,12 @@ class UsersController < ApplicationController
     @user = User.create(params[:user_params])
 
     if @user.valid?
+      @user.write_attribute(:last_name, last_name_refactor(@user[:last_name])) #use the method for the last name
       @user.save
       redirect_to "/users/new", status: 301
     else
-      errors = @user.errors.full_messages
-      redirect_to "/users/new", status: 301, flash: { errors: errors}
+      errors = @user.errors.full_messages # full_message allows to show the list of the errors
+      redirect_to "/users/new", status: 301, flash: { errors: errors} # flash allows to send parameters to the redirect
     end
 
   end
@@ -40,10 +41,34 @@ class UsersController < ApplicationController
   #   temporal = my_last_name.downcase.split(' ')
   #   temporal.each_with_index do |c|
   #     if c.length >= 3
-  #       if c.[0.1] != "d"
+  #       if c.[0,1] != "d"
   #         c.capitalize!
   #       end
   #     end
   #   end
   #   temporal.join(' ')
+
+
+  def last_name_refactor(last_name)
+    last_name_array = last_name.downcase.split(' ')  
+
+    articles = [
+      "de",
+      "del",
+      "la",
+      "las",
+      "los"
+    ]
+
+    last_name_array.map! do |word|
+      if articles.include? word 
+        word
+      else
+        word.capitalize
+      end
+    end
+
+    last_name_array.join(' ')
+  end
+
 end
